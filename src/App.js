222bb23d57a536e5056899d4a1a24a589ed9eb56
JAS <info@aone.pl>
2024-06-20 10:46:36 +0200

    init

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Przygotowujemy plik <code>App.js</code>, strona w przebudowie.
         </p>
         <a
           className="App-link"
