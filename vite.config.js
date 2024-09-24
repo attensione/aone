189d79b55626b424ce9ad512a8254633189e2f7f
JAS <info@aone.pl>
2024-09-24 13:48:07 +0200

    init

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    root: resolve(__dirname, 'src'),
     build: {
-        outDir: './docs',
+        outDir: '../docs',
         emptyOutDir: true,
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'index.html'),
-                nested: resolve(__dirname, 'error.html'),
+                main: resolve(__dirname, 'src/index.html'),
+                nested: resolve(__dirname, 'src/error.html'),
             },
         },
     },
